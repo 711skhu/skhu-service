df116742b97d8fd8021648415706f762e34a3e12
kje6445 <32263346+kje6445@users.noreply.github.com>
2019-09-20 21:17:22 +0900

    Revert "건물 선택 오류 수정 완료"
    This reverts commit fc744892948bc0afdc7e66008b25d6a5e0f49a36.

--- src/main/java/com/shouwn/com/skhuservice/rental/BuildingType.java
+++ src/main/java/com/shouwn/com/skhuservice/rental/BuildingType.java
@@ -12,10 +12,10 @@ public enum BuildingType {
 	SEUNGYEON_HALL(1, "승연관", "gv건물목록_ctl02_btnSelect"),
 	IIMAN_HALL(2, "일만관", "gv건물목록_ctl03_btnSelect"),
 	WOLDANGG_HALL(3, "월당관", "gv건물목록_ctl04_btnSelect"),
-	LEECHUNHWAN_HALL(6, "이천환관", "gv건물목록_ctl06_btnSelect"),
-	SSECHUNNUN_HALL(7, "새천년관", "gv건물목록_ctl07_btnSelect"),
-	SUNGMIELE_HALL(9, "성미가엘성당", "gv건물목록_ctl08_btnSelect"),
-	MIELE_HALL(11, "미가엘관", "gv건물목록_ctl09_btnSelect");
+	LEECHUNHWAN_HALL(6, "이천환관", "gv건물목록_ctl07_btnSelect"),
+	SSECHUNNUN_HALL(7, "새천년관", "gv건물목록_ctl08_btnSelect"),
+	SUNGMIELE_HALL(9, "성미가엘성당", "gv건물목록_ctl09_btnSelect"),
+	MIELE_HALL(11, "미가엘관", "gv건물목록_ctl10_btnSelect");
 
 	private static final Map<Integer, BuildingType> map;
 
